cf234fa2d3694d4166c73671b0d3ab2a1e9fb71d
yap cheah shen <yapcheahshen@gmail.com>
2023-12-07 00:09:31 +0800

    first working demo

---
+++ src/appstore.js
@@ -0,0 +1,18 @@
+export * from 'accelon23/src/store.js';
+import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
+
+_ACC23.CacheName='v1::dhammaprimer',
+_ACC23.AppVer='23.12.6';
+_ACC23.AppName='dhammaprimer'
+_ACC23.AppTitle='達磨小抄'
+_ACC23.allptks=['dhammaprimer'];
+_ACC23.selectedptks=['dhammaprimer']
+_ACC23.activeptk='dhammaprimer'
+_ACC23.ptkinfo={
+    'dhammaprimer':"中文版",
+    'dhammaprimer-en':"英文版",
+}
+_ACC23.favorites=[];
+_ACC23.MAXFAVORITE=100;
+_ACC23.ITEMPERPAGE=5;
+export const ACC23 = _ACC23;
